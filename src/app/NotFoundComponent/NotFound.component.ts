1434cafc4e4ee8ba48a58ee37f0eb2bd49f722f4
Nikita <nikki-xit@mail.ru>
2022-10-20 12:20:59 +0300

    creating main pages styles

---
+++ src/app/NotFoundComponent/NotFound.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'NotFoundSelector',
+    templateUrl: './NotFound.component.html',
+    styleUrls: ['./NotFound.component.scss'],
+})
+export class NotFoundComponent {}
