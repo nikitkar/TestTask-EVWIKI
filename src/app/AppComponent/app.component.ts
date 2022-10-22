d87c6c77fe78fcf5c68acfcca72fc41d454318c4
Nikita <nikki-xit@mail.ru>
2022-10-22 23:22:09 +0300

    creating currency converter

--- src/app/AppComponent/app.component.ts
+++ src/app/AppComponent/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { GetTradingAmountServices } from '../GetTradingAmountViewModel/GetTradingAmountServices';
+
+import { GetExchangeRatesInterface } from '../GetTradingAmountViewModel/GetTradingAmountRates';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    providers: [GetTradingAmountServices],
 })
-export class AppComponent {
-  title = 'TestTask-EVWIKI';
+export class AppComponent implements OnInit {
+    getExchangeRatesList: GetExchangeRatesInterface[] = [];
+
+    onChangeValue(e: number, index: number) {
+        this.getExchangeRatesList.map((item, index1) => {
+            //можно реализовть было ввод только цифр, без вожможности ввода букв
+            if (!e.toString().match(/[^0-9]/g) && e.toString()) {
+                if (item.value?.toString() == 'Nan') return;
+
+                e = Number.parseFloat(
+                    Number.parseFloat(
+                        e.toString().replace(/[^0-9]/g, '')
+                    ).toFixed(5)
+                );
+
+                if (index == index1)
+                    item.value = Number.parseFloat(e.toFixed(5));
+                else
+                    item.value = Number.parseFloat(
+                        (e * item.amount).toFixed(5)
+                    );
+            } else item.value = 0;
+        });
+    }
+
+    constructor(private _httpService: GetTradingAmountServices) {}
+
+    ngOnInit() {
+        this._httpService.getCurrencyExchanges().subscribe({
+            next: (data: GetExchangeRatesInterface[]) =>
+                (this.getExchangeRatesList = data),
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule }   from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './Router/app-routing.module';
 import { AppComponent } from './AppComponent/app.component';
@@ -8,7 +10,7 @@ import { NotFoundComponent } from './NotFoundComponent/NotFound.component';
 
 @NgModule({
     declarations: [AppComponent, NavigatePathComponent, NotFoundComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
     providers: [],
     bootstrap: [NavigatePathComponent],
 })
