5264a329210cfa4dad352b59e50fd57bf0da04fd
Nikita <nikki-xit@mail.ru>
2022-10-24 23:21:54 +0300

    finish creating distance converter

--- src/app/AppComponent/app.component.ts
+++ src/app/AppComponent/app.component.ts
@@ -1,46 +1,104 @@
 import { Component, OnInit } from '@angular/core';
 
 import { GetTradingAmountServices } from '../GetTradingAmountViewModel/GetTradingAmountServices';
-
 import { GetExchangeRatesInterface } from '../GetTradingAmountViewModel/GetTradingAmountRates';
+
+import { GetCoverterDistanceServices } from '../GetCoverterDistance/GetCoverterDistanceService';
+import { GetCoverterDistanceInterface, MathOperationDistance } from '../GetCoverterDistance/GetCoverterDistanceInterface';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    providers: [GetTradingAmountServices],
+    providers: [GetTradingAmountServices, GetCoverterDistanceServices],
 })
 export class AppComponent implements OnInit {
     getExchangeRatesList: GetExchangeRatesInterface[] = [];
+    getDistanceList: GetCoverterDistanceInterface[] = [];
 
-    onChangeValue(e: number, index: number) {
-        this.getExchangeRatesList.map((item, index1) => {
+    bufArrayDistance: {} = {};
+    butArrayDistanceMathOperation: MathOperationDistance[] = [];
+
+    readonly listDistanceName = [
+        'Meters',
+        'Feet',
+        'Inches',
+        'Cm',
+        'Yards',
+        'Kilometers',
+        'Miles',
+    ];
+
+    onChangeExchangeRatesValue(e: number, index: number) {
+        this.getExchangeRatesList.map((item, indexExchangeRates) => {
             //можно реализовть было ввод только цифр, без вожможности ввода букв
-            if (!e.toString().match(/[^0-9]/g) && e.toString()) {
+            if (!e.toString().match(/[^0-9\.]/g) && e.toString()) {
                 if (item.value?.toString() == 'Nan') return;
 
                 e = Number.parseFloat(
                     Number.parseFloat(
-                        e.toString().replace(/[^0-9]/g, '')
+                        e.toString().replace(/[^0-9\.]/g, '')
                     ).toFixed(5)
                 );
 
-                if (index == index1)
-                    item.value = Number.parseFloat(e.toFixed(5));
+                if (index == indexExchangeRates) item.value = Number.parseFloat(e.toFixed(5));
                 else
-                    item.value = Number.parseFloat(
-                        (e * item.amount).toFixed(5)
-                    );
-            } else item.value = 0;
+                    item.value = Number.parseFloat((e * item.amount).toFixed(5));
+            }
+            else item.value = 0;
         });
     }
 
-    constructor(private _httpService: GetTradingAmountServices) {}
+    onChangeDistanceValue(e: number, index: number) {
+        for (let i = 0; i < this.listDistanceName.length; i++) {
+            if (this.listDistanceName[i] == this.getDistanceList[index].code)
+                this.bufArrayDistance = this.getDistanceList[i].nameDistance.ConvertTo;
+        }
+
+        this.getDistanceList.map((item, indexDistanceItem) => {
+            if (!e.toString().match(/[^0-9\.]/g) && e.toString()) {
+                if (item.nameDistance.value?.toString() == 'Nan') return;
+
+                e = Number.parseFloat(Number.parseFloat(e.toString().replace(/[^0-9\.]/g, '')).toFixed(5));
+
+                const keysArray = Object.keys(this.bufArrayDistance);
+                this.butArrayDistanceMathOperation = Object.values(this.bufArrayDistance);
+
+                if (index == indexDistanceItem) {
+                    item.nameDistance.value = Number.parseFloat(e.toFixed(5));
+                }
+                else {
+                    for (let i = 0; i < keysArray.length; i++) {
+                        if (keysArray[i] == item.code) {
+                            switch (this.butArrayDistanceMathOperation[i].operator) {
+                                case '/':
+                                    item.nameDistance.value = Number.parseFloat((e / this.butArrayDistanceMathOperation[i].value).toFixed(5))
+                                    break;
+                                case '*':
+                                    item.nameDistance.value = Number.parseFloat((e * this.butArrayDistanceMathOperation[i].value).toFixed(5))
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    constructor(
+        private _httpTradingAmountService: GetTradingAmountServices,
+        private _httpCoverterDistanceService: GetCoverterDistanceServices
+    ) {}
 
     ngOnInit() {
-        this._httpService.getCurrencyExchanges().subscribe({
+        this._httpTradingAmountService.getCurrencyExchanges().subscribe({
             next: (data: GetExchangeRatesInterface[]) =>
                 (this.getExchangeRatesList = data),
+        });
+
+        this._httpCoverterDistanceService.getDistanceData().subscribe({
+            next: (data: GetCoverterDistanceInterface[]) =>
+                (this.getDistanceList = data),
         });
     }
 }
