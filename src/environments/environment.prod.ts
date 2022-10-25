14cb661e326761ff5a10bbe98df32fd488457101
Nikita <nikki-xit@mail.ru>
2022-10-25 09:05:29 +0300

    add Docker file, creating dist folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  api_key: '093cf7f945614361aad63f9fb8e63b3d',
+  URL: 'https://exchange-rates.abstractapi.com/v1/live',
+  baseСurrencyURL: "USD",
+  testURI: '../assets/dataJson.json',
+  testURIDistance: '../assets/distanceJson.json'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   api_key: '093cf7f945614361aad63f9fb8e63b3d',
   URL: 'https://exchange-rates.abstractapi.com/v1/live',
   baseСurrencyURL: "USD",
